5c6d12d2524ee51c89f0c50b1edab0d5c5cf295a
Velo <105860237+velo-app[bot]@users.noreply.github.com>
2026-06-07 07:12:40 +0000

    Initial commit

---
+++ src/pages/Code Portofolio.u9wa8.js
@@ -0,0 +1,7 @@
+// Velo API Reference: https://www.wix.com/velo/reference/api-overview/introduction
+
+$w.onReady(function () {
+
+"name": "get_url_content", "parameters": { "url": "https://gist.github.com/Zubenesch/7293615137af75a10832f79ced31c5b1.js"
+
+});
---
+++ src/pages/masterPage.js
@@ -0,0 +1,5 @@
+// The code in this file will load on every page of your site
+
+$w.onReady(function () {
+"name": "get_url_content", "parameters": { "url": "https://gist.github.com/Zubenesch/7293615137af75a10832f79ced31c5b1.js"
+});
